f6d08b7023a2accadb8c2cdf33dd56ab737645bb
Tanmay Kanase <157377290+Tanmay-kanase@users.noreply.github.com>
2025-07-20 19:10:48 +0530

    Update server.js

--- client/server.js
+++ client/server.js
@@ -8,7 +8,7 @@ app.listen(8888, () => {
   console.log("server is running on port 8888!!!!!");
 });
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
